d1d30caa3093d24cfdb32346a496be9f56c55ebb
Aayush Virk <aayush140405@gmail.com>
2025-05-06 13:54:29 +0530

    upi doe

--- script.js
+++ script.js
@@ -8,3 +8,6 @@ const footer = "Footer added in our website";
 console.group(footer);
 const payment = "Integrated the payment gateway";
 console.log(payment);
+
+const upi = "Integrated the UPI";
+console.log(upi);
